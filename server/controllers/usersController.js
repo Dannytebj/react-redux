7e547bdf706c14c775fdbd99e02d8fc271b1f50b
Dannytebj <Dannytebj@gmail.com>
2018-01-12 13:38:19 +0100

    [Feature] Setup postgres and save user in database

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,10 +1,22 @@
+import bcrypt from 'bcrypt';
 import validateInput from '../utils/validators/SignUpValidator';
+import User from '../models/user';
 
 exports.signUp = (req, res) => {
   const { errors, isValid } = validateInput(req.body);
-  if (!isValid) {
-    res.status(400).json({ errors });
+  if (isValid) {
+    const { username, password, timezone, email } = req.body
+    const password_digest = bcrypt.hashSync(password, 10);
+    User.forge({
+      username, timezone, email, password_digest
+    }, { hasTimestamps: true }).save()
+      .then(user =>
+        res.status(201).send({ message: 'Successful!', user })
+      )
+      .catch(error =>
+      res.status(500).json({ error })
+      );
   } else {
-    res.status(201).send({ message: 'Successful!' })
+    res.status(400).json({ errors });
   }
 };
